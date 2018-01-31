a126e67e83a5d552ff3c7fb188ef8001c5de064d
MeiTe <823376567@qq.com>
2018-01-31 17:13:13 +0800

    classify

--- js/resume.js
+++ js/resume.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
+$(function(){
 	$('.myStat').circliful();
+	$(".scroll").click(function(event) {
+		event.preventDefault();
+		$('html,body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000);
+	});
 });
