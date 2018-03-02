78cd20a728d4dc20dcb125a3d83dd5878015cd58
MeiTe <823376567@qq.com>
2018-03-02 16:25:35 +0800

    MatchingHigeLight

---
+++ js/heightlihgt.js
@@ -0,0 +1,23 @@
+$(function () {
+    //text higelight
+    $("#findTerms").keyup(function () {
+        var inputVal = $("#findTerms").val();
+        eval("reg=/" + inputVal + "/g");
+        $("#findTermsText p").each(function (index, obj) {
+            var strFirstText = $(obj).text();
+            var strLastText = strFirstText.replace(reg, "<span class='bg-bluebright'>" + inputVal + "</span>");
+            $(obj).html(strLastText);
+        });
+    });
+   
+});
+function navMatching(obj) {
+    var navValue = $(obj).attr("data-value");
+    eval("reg=/" + navValue + "/g");
+    console.log(navValue);
+    $("#findTermsText p").each(function (index, obj) {
+        var strFirstText = $(obj).text();
+        var strLastText = strFirstText.replace(reg, "<span class='bg-bluebright'>" + navValue + "</span>");
+        $(obj).html(strLastText);
+    })
+}
